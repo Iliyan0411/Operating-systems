6eca515d7e2c76b85fa2718f15ccd71e124fd94b
Iliyan0411 <ilian_dd00@abv.bg>
2021-05-18 10:19:10 +0300

    Task 5

---
+++ C/Excercise_12/zad5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+
+
+int main(int argc, char **argv)
+{
+    int m, n;
+    scanf("%d %d", &m, &n);
+
+    if(m > n)
+    {
+        return 0;
+    }
+
+    int size = n - m + 1;
+    char *str = (char *)malloc(sizeof(char) * (size+1));
+
+    int fd = open(argv[1], O_RDONLY);
+    lseek(fd, m, SEEK_SET);
+    
+    read(fd, str, size);
+    str[size] = '\0';
+    printf("%s", str);
+
+    close(fd); 
+    free(str);
+
+    return 0;
+}
