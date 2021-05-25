d0cb94fcef6a56958acf89e6f07e103f19ce1695
Iliyan0411 <ilian_dd00@abv.bg>
2021-05-25 13:46:06 +0300

    Task 4

---
+++ C/Excercise_13/zad4.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <wait.h>
+
+
+int main(int argc, char **argv)
+{
+    int status = 0;
+    int pid = fork();
+
+    if(pid == -1){
+        perror("Problem with fork() command!\n");
+        exit(-1);
+    }
+    else if(pid > 0){
+        wait(&status);
+        printf("\nChild's ID: %d\n", getpid());
+        printf("Exit code: %d\n", status);
+    }
+    else{
+        char **args = (char **)malloc((argc+1) * sizeof(char));
+        for(int i = 0; i < argc-1; i++)
+        {
+            args[i] = argv[i+1];
+        }
+        char end[5] = "NULL";
+        args[argc] = end;
+
+        exit(execvp(argv[1], args));
+    }
+
+
+    return 0;
+}
